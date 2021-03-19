164b22f3981c640305f0d37ab86431a7f5b725be
ukonika <ukonika28@gmail.com>
2021-03-19 13:36:45 +0300

    master

---
+++ app/js/script.js
@@ -0,0 +1,68 @@
+for(let i=9; i>1; i--) {
+    console.log(i**2)
+}
+
+
+
+
+// let a = 2 + 2;
+// // a = prompt("Input your digit", '0');
+// switch (a) {
+//     case 4:
+//         alert ('a');
+//         break;
+//     case 5:
+//         alert ('b');
+//         break;
+//     case 6:  
+//         alert ('c');
+//         break;  
+//     default:
+//         alert ('z');
+//         break;   
+// }
+// console.log(a);
+
+// let a = 1;
+// let n;
+
+// a = prompt("Input your digit", '0');
+// if (a > 0) {
+//   n = true;
+// }
+
+// else {
+//     n = false;
+// }
+
+// console.log(n);
+
+
+
+// let a = 1;
+// let b = 2;
+// let c;
+
+// a = prompt("Input your digit", '0');
+
+// if (a != null){
+//     console.log("ok was pressed");
+//     a = parseInt(a);
+//     if (a > 0 && a < 0) {
+//         c = a + b * 3;
+//     } else {
+//         c = a * 2 + b * 4;
+//     }
+// } 
+
+// else {
+//      console.log("cancel was pressed");
+// }
+
+
+
+// alert(c);
+
+// console.log(c);
+// c++;
+// console.log(c);
